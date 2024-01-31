cddf51511ac434aea12c19bf4b4bcd98d5d3a441
Md.Sohid <mdsohidullahchowdhury1@gmail.com>
2024-01-31 21:24:29 +0600

    Update

--- lib/home.dart
+++ lib/home.dart
@@ -11,98 +11,94 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              height: 100,
-              width: 100,
-              child: CircleAvatar(
-                backgroundImage: AssetImage('image/shakil.jpg'),
-              ),
-              ),
-           
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            child: CircleAvatar(
+              backgroundImage: AssetImage('image/shakil.jpg'),
             ),
-            SizedBox(height: 8,),
-
-            Text("Shakil",
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: 20
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          "Shakil",
+          style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+        ),
+        Text(
+          "Chowdhury",
+          style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(6),
+          child: Card(
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Personality(),
+                    ));
+              },
+              leading: Icon(Icons.group),
+              title: Text('Personality'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              iconColor: Colors.white,
+              tileColor: Colors.orange.shade700,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
             ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(6),
+          child: Card(
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SubWork(),
+                    ));
+              },
+              leading: Icon(Icons.work_outlined),
+              title: Text("Work ToDay's"),
+              trailing: Icon(Icons.arrow_forward_ios),
+              iconColor: Colors.white,
+              tileColor: Colors.deepPurple.shade600,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
             ),
-
-            Text("Chowdhury",
-            style: TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: 20
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(6),
+          child: Card(
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Setting(),
+                    ));
+              },
+              leading: Icon(Icons.settings),
+              title: Text('Setting'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              iconColor: Colors.white,
+              tileColor: Colors.blueAccent.shade700,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
             ),
-            ),
-            
-      SizedBox(height: 8,),      
-            
-                
-                Padding(
-                  padding: const EdgeInsets.all(6),
-                  child:Card(
-                    child: ListTile(
-                      
-                      onTap:  (){
-                      Navigator.push(context, MaterialPageRoute(builder:(context)=>Personality(),));
-                                   },
-                      
-                      leading: Icon(Icons.group),
-                      title: Text('Personality'), 
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      iconColor: Colors.white,
-                      tileColor: Colors.orange,
-                    ),
-                    ),
-                      ),
-
-                      Padding(
-                  padding: const EdgeInsets.all(6),
-                  child:Card(
-                    child: ListTile(
-                      
-                      onTap:  (){
-                      Navigator.push(context, MaterialPageRoute(builder:(context)=>SubWork(),));
-                                   },
-                    
-                      leading: Icon(Icons.note),
-                      title: Text("Work ToDay's"),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      iconColor: Colors.white,
-                      tileColor: Colors.deepPurple,
-                      
-                    ),
-                    ),
-                      ),
-
-                      Padding(
-                  padding: const EdgeInsets.all(6),
-                  child:Card(
-                    child: ListTile(
-                      
-                      onTap:  (){
-                      Navigator.push(context, MaterialPageRoute(builder:(context)=>Setting(),));
-                                   },
-                    
-                      leading: Icon(Icons.settings),
-                      title: Text('Setting'),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      iconColor: Colors.white,
-                      tileColor: Colors.blueAccent,
-                    ),
-                    ),
-                      ),
-              
-              ]
-
-            
-            
-  ),
-);
-}
+          ),
+        ),
+      ]),
+    );
+  }
 }
--- lib/setting.dart
+++ lib/setting.dart
@@ -11,7 +11,6 @@ class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: Column(
@@ -19,95 +18,113 @@ class Setting extends StatelessWidget {
           children: [
             Row(
               children: [
-                Text("Settings",
+                Text(
+                  "Settings",
                   style: TextStyle(
-                  fontSize: 26,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: AutofillHints.countryName ),
-                  ),SizedBox(height: 8,),  
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: AutofillHints.countryName),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
               ],
             ),
-             Row(
-               children: [SizedBox(height: 4,),
-                 Text("Your setting so that we are comfortable"),
-               ],
-             ),
-  Padding(
-                  padding: const EdgeInsets.all(6),
-                  child:Card(
-                    child: ListTile(
-                      
-                      onTap:  (){
-                      Navigator.push(context, MaterialPageRoute(builder:(context)=>Personality(),));
-                                   },
-                    
-                      
-                      title: Center(child: Text('Personality')),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      iconColor: Colors.white,
-                      tileColor: Colors.greenAccent,
-                    ),
-                    ),
-                    ),
-  Padding(
-                  padding: const EdgeInsets.all(6),
-                  child:Card(
-                    child: ListTile(
-                      
-                      onTap:  (){
-                      Navigator.push(context, MaterialPageRoute(builder:(context)=>Language(),));
-                                   },
-                    
-                      
-                      title: Center(child: Text('Language')),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      iconColor: Colors.white,
-                      tileColor: Colors.blueAccent,
-                    ),
-                    ),
-                    ),
-  Padding(
-                  padding: const EdgeInsets.all(6),
-                  child:Card(
-                    child: ListTile(
-                      
-                      onTap:  (){
-                      Navigator.push(context, MaterialPageRoute(builder:(context)=>Setting(),));
-                                   },
-                    
-                      
-                      title: Center(child: Text('Terms and Condition',)),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      iconColor: Colors.white,
-                      tileColor: Colors.pinkAccent,
-                    ),
-                    ),
-                    ),
-
-                    Padding(
-                  padding: const EdgeInsets.all(6),
-                  child:Card(
-                    child: ListTile(
-                      
-                      onTap:  (){
-                      Navigator.push(context, MaterialPageRoute(builder:(context)=>LogIn(),));
-                                   },
-                    
-                      leading: Icon(Icons.login_outlined),
-                      title: Center(child: Text('Log-Out')),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      iconColor: Colors.black45,
-                      tileColor: Colors.red,
-                    ),
-                    ),
-                      ),
-              
-                                  
+            Row(
+              children: [
+                SizedBox(
+                  height: 4,
+                ),
+                Text("Your setting so that we are comfortable"),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(6),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Personality(),
+                        ));
+                  },
+                  title: Center(child: Text('Personality')),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  iconColor: Colors.white,
+                  tileColor: Colors.greenAccent.shade700,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(6),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Language(),
+                        ));
+                  },
+                  title: Center(child: Text('Language')),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  iconColor: Colors.white,
+                  tileColor: Colors.blueAccent.shade700,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(6),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Setting(),
+                        ));
+                  },
+                  title: Center(
+                      child: Text(
+                    'Terms and Condition',
+                  )),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  iconColor: Colors.white,
+                  tileColor: Colors.pinkAccent.shade700,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(6),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LogIn(),
+                        ));
+                  },
+                  leading: Icon(Icons.login_outlined),
+                  title: Center(child: Text('Log-Out')),
+                  trailing: Icon(Icons.arrow_forward_ios),
+                  iconColor: Colors.black45,
+                  tileColor: Colors.red.shade400,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+              ),
+            ),
           ],
-          
         ),
       ),
-      
     );
   }
 }
